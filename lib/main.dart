733303013f375402e946fe41653ae4a82af7921d
Rafael <rafael.andrade.dev0@gmail.com>
2019-09-29 22:11:27 -0300

    Alterando visual e adicionando sons

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Expanded createCardSound(int imageNumber) {
+  final player = AudioCache();
+  void playSound(int soundNumber) {
+    player.play('sound$soundNumber.mp3');
+  }
+
+  void stopSound(int soundNumber) {
+    player.clear('sound$soundNumber.mp3');
+    print('Stop Sound $soundNumber');
+  }
+
+  Expanded createCardSound(int number) {
     return Expanded(
       child: GestureDetector(
-        onTap: () => print('Tap Card'),
+        onTap: () {
+          playSound(number);
+        },
+        onLongPress: () {
+          stopSound(number);
+        },
         child: Card(
           elevation: 5,
           margin: EdgeInsets.all(10),
           clipBehavior: Clip.antiAliasWithSaveLayer,
           child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('images/$imageNumber.jpg'),
-                    fit: BoxFit.fill),
-              ),
-              child: ListTile(
-                title: Text('Titulo do Card'),
-                subtitle: Text('Subitulo do Card'),
-              )),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/$number.jpg'), fit: BoxFit.fill),
+            ),
+          ),
         ),
       ),
     );
